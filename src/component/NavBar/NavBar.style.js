e9d564a8354761332f57cfe3d04ad4c4f514d213
Yoon Chang <yoonsees@gmail.com>
2019-07-15 12:29:24 -0500

    Update NavItem hover actions

--- src/component/NavBar/NavBar.style.js
+++ src/component/NavBar/NavBar.style.js
@@ -1,6 +1,12 @@
 import styled from "styled-components"
 import { Flex } from "../../style/grid"
-import { midBlue, gray, midGray, darkGray } from "../../style/constants"
+import {
+  midBlue,
+  darkBlue,
+  gray,
+  midGray,
+  darkGray
+} from "../../style/constants"
 
 export const NavBarContainer = styled(Flex)`
   flex-direction: row;
@@ -10,7 +16,6 @@ export const NavBarContainer = styled(Flex)`
   min-height: fit-content;
   top: 0;
   left: 0;
-  /* justify-content: center; */
   align-items: center;
   background-color: ${midBlue};
   padding-left: 40px;
@@ -33,55 +38,32 @@ export const NavBarContainer = styled(Flex)`
 
 export const NavItemContainer = styled(Flex)`
   flex-direction: row;
-  /* padding: 10px; */
   justify-content: flex-start;
   align-items: center;
   height: 100%;
-
-  h1 {
-    margin-left: 10px;
-    margin-right: 10px;
-    color: white;
-  }
 `
 
 export const NavItem = styled.button`
   position: relative;
-  /* margin-top: auto;
-  margin-bottom: 20px;
-  height: 5%; */
   background-color: transparent;
+  height: 100%;
   font-weight: 600;
   font-size: 1em;
-  /* border-radius: 15px; */
   padding: 20px;
-  margin-left: 5px;
-  margin-right: 5px;
   align-items: center;
   justify-content: center;
   border: none;
   cursor: pointer;
   color: white;
-
-  /* transform-origin: right; */
   transition: all 0.2s;
-  /* color: ${midGray}; */
-  color: white;
-
-  &:hover {
-    color: ${midGray};
-    transform: scale(1.1);
-  }
 `
 
 export const HoverList = styled.ul`
   display: none;
   opacity: 0;
-  /* position: absolute; */
-  /* z-index: 99; */
   position: fixed;
   top: 10%;
-  right: 120px;
+  right: 109px;
   flex-direction: column;
   border: none;
   background-color: ${midBlue};
@@ -91,8 +73,7 @@ export const HoverList = styled.ul`
     display: flex;
     ${NavItem} {
       &:hover {
-        background-color: ${midGray};
-        color: black;
+        background-color: ${darkBlue};
       }
     }
   }
@@ -105,30 +86,20 @@ export const TriggerItem = styled.button`
   background-color: transparent;
   position: relative;
   height: 100%;
-  /* margin-top: auto;
-  margin-bottom: 20px;
-  height: 5%; */
   background-color: transparent;
   font-weight: 600;
   font-size: 1em;
-  /* border-radius: 15px; */
   padding: 10px;
-  margin-left: 5px;
-  margin-right: 5px;
   align-items: center;
   justify-content: center;
   border: none;
   cursor: pointer;
   color: white;
-
-  /* transform-origin: right; */
   transition: all 0.2s;
-  /* color: ${midGray}; */
-  color: white;
   &:hover {
     ${HoverList} {
       display: flex;
-    opacity: 1;
+      opacity: 1;
     }
   }
 `
